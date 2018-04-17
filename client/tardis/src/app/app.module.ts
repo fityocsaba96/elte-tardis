0e4aa123598e5d617e1c13a502e04d7d3661ff2d
Fityó Csaba <fityocsaba96@gmail.com>
2018-04-17 14:19:25 +0200

    configure linters, add them to Travis and fix linting errors

--- client/tardis/src/app/app.module.ts
+++ client/tardis/src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
