49a872a6d5885164503f9bd401e554a8058eaa33
tema <tempfliakos@inf.elte.hu>
2018-04-28 20:25:28 +0200

    search and draw as table

--- client/tardis/src/app/components/adding-subjects/adding-subjects.component.ts
+++ client/tardis/src/app/components/adding-subjects/adding-subjects.component.ts
@@ -13,35 +13,46 @@ export class AddingSubjectsComponent implements OnInit{
   search: string;
   subjects: Subject[];
   ttkURL: string;
+  parser: DOMParser;
 
   constructor(private addingSubjectService: AddingSubjectService) { }
 
   ngOnInit() {
     this.search = '';
     this.subjects = [];
+    this.parser = new DOMParser();
   }
 
   getSubject(subjectName: string) {
     this.addingSubjectService.getData(subjectName)
     .subscribe( (res) => {
-      this.parseHtml(),
-      console.log(this.subjects)
+      this.parseHtml(res)
     }, (err) => {
       console.log(err);
     });
   }
 
-  parseHtml(): Document {
-    const parser = new DOMParser();
-    const table = document.querySelector('table');
+  parseHtml(html: string): Document {
+    const document = this.parser.parseFromString(html,'text/html');
+    const table = document.querySelector('tbody');
     if (table == null) {
       return;
     }
     const rows = Array.from(table.querySelectorAll('tr'));
     rows.forEach((row) => {
-        const nameCell = row.querySelector('td');
         this.subjects.push({
-          kurzusnev: nameCell.innerText.trim()
+          kurzusnev: row.cells[0].innerText.trim(),
+          kurzuskod: row.cells[1].innerText.trim(),
+          idopont: row.cells[2].innerText.trim(),
+          helyszin: row.cells[3].innerText.trim(),
+          hetek: row.cells[4].innerText.trim(),
+          megj: row.cells[5].innerText.trim(),
+          oratipus: row.cells[6].innerText.trim(),
+          csop: row.cells[7].innerText.trim(),
+          letszam: row.cells[8].innerText.trim(),
+          ea: row.cells[9].innerText.trim(),
+          gyak: row.cells[10].innerText.trim(),
+          oktato: row.cells[11].innerText.trim()
         });
     });
     return document;
--- client/tardis/src/app/models/subject.ts
+++ client/tardis/src/app/models/subject.ts
@@ -1,13 +1,15 @@
-export interface Subject {
-    kurzusnev: string;
-    /*kurzuskod: string;
-    idopont: string;
-    helyszin: string;*/
-    /*megj: string;
-    oratipus: string;
-    csop: string;
-    letszam: string;
-    ea: string;
-    gyak: string;
-    oktato: string;*/
+export class Subject {
+    constructor(
+    kurzusnev: string,
+    kurzuskod: string,
+    idopont: string,
+    helyszin: string,
+    hetek:string,
+    megj: string,
+    oratipus: string,
+    csop: string,
+    letszam: string,
+    ea: string,
+    gyak: string,
+    oktato: string) { }
 }
