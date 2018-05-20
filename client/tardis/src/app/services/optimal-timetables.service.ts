3507985a9c5e4aebd67c562c92f6709a28cf9704
Fityó Csaba <fityocsaba96@gmail.com>
2018-05-20 21:19:25 +0200

    extend algorithm to handle the 3 simple conditions, add tests to algorithm

--- client/tardis/src/app/services/optimal-timetables.service.ts
+++ client/tardis/src/app/services/optimal-timetables.service.ts
@@ -1,10 +1,14 @@
 import {Injectable} from '@angular/core';
 import {ICourse} from '../models/ICourse';
+import {IFreeTime} from '../models/IFreeTime';
 import {ISubject} from '../models/ISubject';
 import {ITime} from '../models/ITime';
 import {ITimetable} from '../models/ITimetable';
 import {ITimetableCourse} from '../models/ITimetableCourse';
+import {EarliestStartService} from './earliest-start.service';
 import {FreeTimeService} from './free-time.service';
+import {LatestEndService} from './latest-end.service';
+import {LongestBreakService} from './longest-break.service';
 import {MarkmyprofessorRatingService} from './markmyprofessor-rating.service';
 import {SubjectService} from './subject.service';
 import {TimeService} from './time.service';
@@ -16,7 +20,10 @@ export class OptimalTimetablesService {
 
   constructor(private markmyprofessorRatingService: MarkmyprofessorRatingService,
               private subjectService: SubjectService,
-              private freeTimeService: FreeTimeService) {
+              private freeTimeService: FreeTimeService,
+              private earliestStartService: EarliestStartService,
+              private latestEndService: LatestEndService,
+              private longestBreakService: LongestBreakService) {
     this._optimalTimetables = [];
   }
 
@@ -33,11 +40,19 @@ export class OptimalTimetablesService {
         conflicted: [],
       },
     ];
+
     for (const subject of this.subjectService.subjects.notConflicted) {
       const newTimetables: ITimetable[] = [];
       for (const course of subject.courses) {
-        if (!this.markmyprofessorRatingService.apply
-        || (await this.markmyprofessorRatingService.meetsCondition(course.professor.name)) === true) { // TODO: C és D feltétel ellenőrzése
+
+        const markmyprofessorConditionOk = !this.markmyprofessorRatingService.apply
+          || (await this.markmyprofessorRatingService.meetsCondition(course.professor.name));
+        const earliestStartConditionOk = !this.earliestStartService.apply
+          || this.earliestStartService.meetsCondition(course.time);
+        const latestEndConditionOk = !this.latestEndService.apply
+          || this.latestEndService.meetsCondition(course.time);
+
+        if (markmyprofessorConditionOk && earliestStartConditionOk && latestEndConditionOk) {
           if (this.markmyprofessorRatingService.apply) {
             course.professor.rating = this.markmyprofessorRatingService.filterProfessors()
               .find((professor) => professor.name === course.professor.name).rating;
@@ -57,7 +72,31 @@ export class OptimalTimetablesService {
       }
       timetables = newTimetables;
     }
-    // TODO: E feltétellal kapcsolatos elágazás
+
+    if (this.longestBreakService.apply) {
+      const newTimetables: ITimetable[] = [];
+      for (const timetable of timetables) {
+        const datesOfFreeTime = timetable.freeTime.map((freeTime: IFreeTime) => freeTime.time);
+        const datesOfNotConflicted = timetable.notConflicted.map((course: ITimetableCourse) => course.time);
+        const allDates = datesOfFreeTime.concat(datesOfNotConflicted);
+        let allPairsConditionOk = true;
+        allDates.sort((timeA: ITime, timeB: ITime) => {
+          if (timeA.day === timeB.day) {
+            return timeA.startTime < timeB.startTime ? -1 : 1;
+          } else {
+            return timeA.day < timeB.day ? -1 : 1;
+          }
+        });
+        for (let i = 0; i < allDates.length - 1; i++) {
+          allPairsConditionOk = allPairsConditionOk && this.longestBreakService.meetsCondition(allDates[i], allDates[i + 1]);
+        }
+        if (allPairsConditionOk) {
+          newTimetables.push(timetable);
+        }
+      }
+      timetables = newTimetables;
+    }
+
     for (const subject of this.subjectService.subjects.conflicted) {
       const newTimetables: ITimetable[] = [];
       for (const course of subject.courses) {
